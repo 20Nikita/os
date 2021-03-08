7eb68955facb6090d5a231733ea2d31d82b79f9d
20Nikita <gordeev.2017@br.ru>
2021-03-09 01:32:20 +0300

    Добавил чтение из файла

--- fLabProg/src/js/main.js
+++ fLabProg/src/js/main.js
@@ -7,15 +7,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const cellWrapper = document.querySelector(".task-wrapper");
     const tactWrapper = document.querySelector(".tact-wrapper");
-
     // let data = generate(20);
     // let processedData = fifoAlgo(data);
     // paintNums(processedData, tactWrapper);
     // paintString(processedData, cellWrapper);
 
-    let data = strf(generate(20));
-    let processedData = fifoAlgo(data)
-    paintNums(processedData, tactWrapper);
-    paintString(processedData, cellWrapper);
+    // let data = strf(generate(20));
+    // let processedData = fifoAlgo(data)
+    // paintNums(processedData, tactWrapper);
+    // paintString(processedData, cellWrapper);
     
 });
--- fLabProg/src/js/preprocess.js
+++ fLabProg/src/js/preprocess.js
@@ -1,16 +1,27 @@
-
-export function readFile(input) {
-
-    let reader = new FileReader();
-  
-    reader.readAsText(input);
-  
+document.getElementById("myBtn").addEventListener("click", function() {
+  let boof = ["sfesr"]
+  let reader = new FileReader();
+  reader.addEventListener('load', function() {
+    document.getElementById('file').innerText = this.result;
+  });
+  reader.readAsText(document.querySelector('input').files[0]);
     reader.onload = function() {
-      console.log(reader.result);
+      boof = reader.result.split("\n")
+      let mainArr = [];
+      for (let i in boof){
+        let t = boof[i].split(", ")
+        t[3]= t[3].split(";")[0]
+        let data = [];
+        data.id = t[0];
+        data.readyTime = Number(t[1])
+        data.workTime = Number(t[2])
+        data.prior = Number(t[3])
+        mainArr.push(data);
+        }
+        console.log(mainArr)
     };
-  
     reader.onerror = function() {
       console.log(reader.error);
+      return 1
     };
-  
-  }
+});
