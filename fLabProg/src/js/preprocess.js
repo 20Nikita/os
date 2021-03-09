0e589f15c0d59a86d417989fc7e20c00f3a8650e
20Nikita <gordeev.2017@br.ru>
2021-03-09 09:36:15 +0300

    Добавил комментарии

--- fLabProg/src/js/preprocess.js
+++ fLabProg/src/js/preprocess.js
@@ -1,25 +1,33 @@
+// срабатывает по нажатию кнопки myBtn 
 document.getElementById("myBtn").addEventListener("click", function() {
-  let boof = ["sfesr"]
-  let reader = new FileReader();
+  // создать файловую переменную
+  let reader = new FileReader();      
+
+  // загрузить текст из первого загруженного пользователем файла в переменную reader
   reader.addEventListener('load', function() {
     document.getElementById('file').innerText = this.result;
   });
   reader.readAsText(document.querySelector('input').files[0]);
+
+  // срабатывает после загрузки файла
     reader.onload = function() {
+      // разбить текст в файле на масив строк
       boof = reader.result.split("\n")
-      let mainArr = [];
+      let mainArr = [];               // нада сделать глобальной переменной
+      // пройтись по каждой строке
       for (let i in boof){
-        let t = boof[i].split(", ")
-        t[3]= t[3].split(";")[0]
-        let data = [];
-        data.id = t[0];
-        data.readyTime = Number(t[1])
-        data.workTime = Number(t[2])
-        data.prior = Number(t[3])
-        mainArr.push(data);
+        
+        let t = boof[i].split(", ")   // разбить строку на подстроки
+        t[3]= t[3].split(";")[0]      // удалить символ ;
+        let data = [];                // создать бач
+        data.id = t[0];               // добавить в него id
+        data.readyTime = Number(t[1]) // добавить в него время подачи заявки
+        data.workTime = Number(t[2])  // добавить в него время работы
+        data.prior = Number(t[3])     // добавить в него приоритет
+        mainArr.push(data);           // загрузить элемент бача в массив
         }
-        console.log(mainArr)
     };
+    // не получилось прочитать файл
     reader.onerror = function() {
       console.log(reader.error);
       return 1
--- fLabProg/src/js/strf.js
+++ fLabProg/src/js/strf.js
@@ -1,24 +1,26 @@
 import { generate } from "./helpers";
 
 export function strf (array) {
-let array1 = array.slice()
-console.log(array1)
-let globalTime = 0
-array.sort((a,b) => a["readyTime"] - b["readyTime"])
-for (let i = 0; i < array.length; i++){
-    
-    for (let j = i; j < array.length; j++){
 
-        if (globalTime < array[j]["readyTime"]){
-            break
+    let globalTime = 0                                    // глобальное время, текущий момент времини
+    array.sort((a,b) => a["readyTime"] - b["readyTime"])  // сортировка масива по времини подачи заявки
+
+    for (let i = 0; i < array.length; i++){               // проход по всему масиву
+        for (let j = i; j < array.length; j++){           // проход по не отсартированной части
+            // если заявка от элемента еще не поступила то не расматриваем его
+            if (globalTime < array[j]["readyTime"]){
+                break
+            }
+            // сортируем по времини работы
+            if (array[i]["workTime"] > array[j]["workTime"]){
+                let t = array[i]
+                array[i] = array[j]
+                array[j] = t
+            }
         }
-        if (array[i]["workTime"] > array[j]["workTime"]){
-            let t = array[i]
-            array[i] = array[j]
-            array[j] = t
-        }
+        // выполняем следующюю команду
+        globalTime += array[i]["workTime"]
     }
-    globalTime += array[i]["workTime"]
-}
-return array
+    
+    return array
 }
