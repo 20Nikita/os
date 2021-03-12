f1aafeacb932b950576ff415a6ef0f9a267eb454
AleksandrKrivonos <A.KRIVONOS@didlogic.com>
2021-03-12 12:52:28 +0300

    add input dor native generator

--- fLabProg/src/js/main.js
+++ fLabProg/src/js/main.js
@@ -1,5 +1,6 @@
 import {algoProcess} from "./modules/algoProcess";
 import {loadToInput} from "./modules/loadFileProcess";
+import {checkNums, validateInput} from "./helper/helpers";
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict"
@@ -7,8 +8,26 @@ window.addEventListener('DOMContentLoaded', () => {
     const generateBtn = document.querySelector(".btn-generate"),
         uploadBtn = document.querySelector(".btn-upload");
 
+    let inputToValidate = document.querySelector(".generate-area");
+
+    let isValidField = false;
+
+    inputToValidate.addEventListener("input", function () {
+        let val = this.value.replace(/^0|\D/g, '');
+        this.value = val;
+
+        console.log(this.value.length)
+        if(this.value.length >= 1){
+            isValidField = true;
+        }
+        else{
+            isValidField = false;
+        }
+
+        if(isValidField) algoProcess(generateBtn);                       //запускаем моделирование для данных, сгенерированных на клиенте
+    })
+
     loadToInput();
 
-    algoProcess(generateBtn);                       //запускаем моделирование для данных, сгенерированных на клиенте
     algoProcess(uploadBtn, true);     //запускаем моделирование для данных, загруженных из файла
 });
